a1bd0d277e6364816fa3d2c86068e87660cbab34
hbf <hbfairess@gmail.com>
2016-10-31 16:06:48 -0700

    still fighting getting the value of the selected radio button into the jscript

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,9 @@
-$(function(){
-  $("#no").click(function() {
-    $("#warning").hide();
-    $("#sorry").show();
-  });
-  $("#yes").click(function() {
-    $("#warning").hide();
-    $("#webpage").show();
-  });
-  $("#blanks form").submit(function(event) {
-    var name = $("input#name").val();
-    var age = parseInt($("input#age").val());
-    var genPref = $("input:radio[name=genPref]:checked").val();
-    var polPref = $("select#polPref").val();
-
+$(document).ready(function(event) {
+  $("select").submit(function() {
+    var questions = ("questions:checked").val();
+    alert("got to the beginning of the form submit");
     event.preventDefault();
-  })
+    console.log(selection);
+  });
 
 });
